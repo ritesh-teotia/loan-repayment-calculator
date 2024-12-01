7cc3d418bb34347f88adc7e2ee6f7c73187f019c
riteshteotia <riteshteotia@users.noreply.github.com>
2024-12-01 16:21:33 +0530

    Removing redundant write_csv code

--- service/CalculateLoanTerm.py
+++ service/CalculateLoanTerm.py
@@ -87,26 +87,3 @@ def calculate_loan_term(principal, annual_interest_rate, loan_tenure_years, extr
 
     total_money_saved = (emi * n) - total_payment  # Calculate total money saved
     return data, max(total_money_saved, 0)
-
-
-def write_loan_data_to_csv(file_name, principal, loan_data, loan_tenure_years, extra_emi_percent, total_money_saved):
-    with open(file_name, 'w', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(['Principal Amount', f'{format_number(principal)} '])
-        csvwriter.writerow([])
-        csvwriter.writerow(['Original Tenure', f'{loan_tenure_years} years'])
-        csvwriter.writerow([])
-        csvwriter.writerow([])
-        csvwriter.writerow(
-            ['Normal EMI', f'Extra EMI({extra_emi_percent}%)', 'Yearly Payment', 'Total Period (Years)', '(Months)'])
-        csvwriter.writerows(loan_data)
-
-        # Write the total money saved
-        csvwriter.writerow([])
-        csvwriter.writerow(['Total Money Saved:', format_number(round(total_money_saved, 2))])
-        # Write the total amount submitted towards the loan
-        total_payment = sum(float(row[2].replace(',', '')) for row in loan_data)
-        csvwriter.writerow(['Total Amount Submitted:', format_number(round(total_payment, 2))])
-        # Write the total interest paid
-        total_interest = total_payment - principal
-        csvwriter.writerow(['Total Interest Paid:', format_number(round(total_interest, 2))])
