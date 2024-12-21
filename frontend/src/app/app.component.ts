301b27bf07ec5ba239e142d8e3e043492eccc527
riteshteotia <ritesh-teotia@users.noreply.github.com>
2024-12-21 22:27:33 +0530

    Adding detail component for right side

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LoanFormComponent } from './loan-form/loan-form.component';
+import { LoanDetailComponent } from './loan-detail/loan-detail.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [ LoanFormComponent]
+  imports: [ LoanFormComponent, LoanDetailComponent]
 })
 export class AppComponent {
 }
---
+++ frontend/src/app/loan-detail/loan-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loan-detail',
+  imports: [],
+  templateUrl: './loan-detail.component.html',
+  styleUrl: './loan-detail.component.scss'
+})
+export class LoanDetailComponent {
+
+}
